66902545500037647ff35bb3f3e58bb18b0a0e78
minamo <minamobuusuka@gmail.com>
2016-11-12 16:18:33 +0900

    CalcApp

--- app/src/main/java/jp/techacademy/toyo/mie/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/toyo/mie/calcapp/SecondActivity.java
@@ -21,7 +21,7 @@ public class SecondActivity extends AppCompatActivity {
          double value4 = intent.getDoubleExtra("VALUE",0.0);
 
 
-        TextView textView = (TextView) findViewById(R.id.textView);
+        TextView textView = (TextView) findViewById(R.id.textView1);
         textView.setText(String.valueOf(value1 + value2 + value3 + value4));
     }
 }
